9b0be514959638098ad73b4d3a41b3ed025971ed
Joongi Kim <joongi@lablup.com>
2020-02-05 00:20:16 +0900

    Chage the package name consistentn with the repository name

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='graphqllexer',
+    name='pygments-graphql-lexer',
+    version='0.1',
     packages=find_packages(),
     entry_points=
     """
